c141ec55240a034de747471c99ca839bc83d8e5d
Jonathan Bloedow <jonathan.bloedow@gatesfoundation.org>
2025-04-17 11:09:11 -0700

    Compliance

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -1,6 +1,5 @@
 # calib_job_config.py
 
-#study_name = "nigeria_fullscale_calibrating"
 study_name = "test_polio_calib_fixed"
 num_trials = 1
 parallelism = 4
@@ -8,4 +7,13 @@ completions = 20
 namespace = "default"
 job_name = "laser-polio-worker-jb"
 image = "idm-docker-staging.packages.idmod.org/laser/laser-polio:latest"
-storage_url = REPLACE_WITH_VALUE_FROM_DOCS
+
+# Placeholder
+storage_url = "REPLACE_WITH_VALUE_FROM_DOCS"
+
+if storage_url == "REPLACE_WITH_VALUE_FROM_DOCS":
+    raise RuntimeError(
+        "\n[ERROR] You must set `storage_url` in calib_job_config.py!\n"
+        "Follow the instructions in the documentation to get the correct value.\n"
+        "Hint: It's probably an Azure blob storage URL with credentials.\n"
+    )
--- calib/cloud/report_calib_aks.py
+++ calib/cloud/report_calib_aks.py
@@ -1,17 +1,16 @@
 import sys
 from pathlib import Path
 
+import cloud_calib_config as cfg
 import optuna
 
-import cloud_calib_config as cfg
-sys.path.append(str(Path(__file__).resolve().parent.parent)) 
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 from calib_report import plot_stuff
 from calib_report import save_study_results
 
-
-#STORAGE_URL2 = "mysql+pymysql://root@127.0.0.1:3306/optuna_db"
+# STORAGE_URL2 = "mysql+pymysql://root@127.0.0.1:3306/optuna_db"
 IMAGE_NAME = "idm-docker-staging.packages.idmod.org/laser/laser-polio:latest"
-#study_name = "calib_demo_nigeria2"
+# study_name = "calib_demo_nigeria2"
 study = optuna.load_study(study_name=cfg.study_name, storage=cfg.storage_url)
 study.storage_url = cfg.storage_url
 study.study_name = cfg.study_name
