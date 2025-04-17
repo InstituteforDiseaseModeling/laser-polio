aa886eaa15d67241b8babe5a2f9e99ff2af5d4d1
Jonathan Bloedow <jonathan.bloedow@gatesfoundation.org>
2025-04-17 10:29:23 -0700

    Moved from long yaml to short .py file for settings.

---
+++ calib/cloud/cloud_calib_config.py
@@ -0,0 +1,9 @@
+# calib_job_config.py
+
+study_name = "nigeria_fullscale_calibrating"
+num_trials = 1
+parallelism = 4
+completions = 20
+namespace = "default"
+job_name = "laser-polio-worker-jb"
+image = "idm-docker-staging.packages.idmod.org/laser/laser-polio:latest"
