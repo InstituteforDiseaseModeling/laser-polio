3485526817380ba7689644036469d1a9605f07b2
Jonathan Bloedow <jonathan.bloedow@gatesfoundation.org>
2025-04-17 11:01:34 -0700

    Added script to get report from aks.

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -1,9 +1,11 @@
 # calib_job_config.py
 
-study_name = "nigeria_fullscale_calibrating"
+#study_name = "nigeria_fullscale_calibrating"
+study_name = "test_polio_calib_fixed"
 num_trials = 1
 parallelism = 4
 completions = 20
 namespace = "default"
 job_name = "laser-polio-worker-jb"
 image = "idm-docker-staging.packages.idmod.org/laser/laser-polio:latest"
+storage_url = REPLACE_WITH_VALUE_FROM_DOCS
